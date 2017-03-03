ac73dd1f4d1937aac42f08b553afea9a6ab95802
Shashank Jaiswal <shashank.jaiswal@cuelogic.com>
2017-03-03 19:57:53 +0530

    updates after code review

--- src/app/component/Register.js
+++ src/app/component/Register.js
@@ -12,50 +12,47 @@ export class Register extends React.Component {
             useremail: '',
             username: '',
             password: ''
-                      
+
+
         }
-        this.setUserName = this.setUserName.bind(this);
+        this.saveDetails = this.saveDetails.bind(this);
+        this.handleOnchange = this.handleOnchange.bind(this);
 
     }
 
 
-    setUserName(event) {
-        this.setState({
-            username: event.target.value
-        });
-    }
+ 
 
-    setPassword(event) {
-        this.setState({
-            password: event.target.value
-        });
-    }
-
-    setEmail(event) {
-        this.setState({
-            useremail: event.target.value
-        });
+    handleOnchange(event) {
+        var field = event.target.name;
+        var change={};
+        change[field] = event.target.value;
+        console.log(field)
+        this.setState(change);
     }
 
     saveDetails() {
-       
+        alert('user name:'+this.state.username+' user pass:'+this.state.password+' user email'+(this.state.useremail));
+        
     }
 
     render() {
         return (
             <div >
-                <h2 >Register</h2>
-                <Link to={"/"}>Dashboard</Link><hr />
-                <InputElement inputType={'text'} fieldname={'username'}
-                    placeholder={'User Name'} value={this.state.username} controlFunc={this.setUserName} />
-                <hr />
-                <InputElement inputType={'text'} fieldname={'useremail'}
-                    placeholder={'Email Address'} value={this.state.useremail} controlFunc={(event) => this.setEmail(event)} />
-                <hr />
-                <InputElement inputType={'password'} fieldname={'password'}
-                    placeholder={'Password'} value={this.state.password} controlFunc={(event) => this.setPassword(event)} />
-                <hr />
-                <button type="submit" onClick={() => this.saveDetails()}>Save</button>
+                <form onSubmit={this.saveDetails} >
+                    <h2 >Register</h2>
+                    <Link to={"/"}>Dashboard</Link><hr />
+                    <InputElement inputType={'text'} fieldname={'username'}
+                        placeholder={'User Name'} value={this.state.username} controlFunc={this.handleOnchange} />
+                    <hr />
+                    <InputElement inputType={'text'} fieldname={'useremail'}
+                        placeholder={'Email Address'} value={this.state.useremail} controlFunc={this.handleOnchange} />
+                    <hr />
+                    <InputElement inputType={'password'} fieldname={'password'}
+                        placeholder={'Password'} value={this.state.password} controlFunc={this.handleOnchange} />
+                    <hr />
+                    <button>Save</button>
+                </form>
             </div>
         );
     }
